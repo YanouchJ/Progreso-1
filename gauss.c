940800b365655afbfe647e5c6ff2ce1f6a7e5ae4
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-10-16 16:22:08 -0500

    Declaración de variables y codificación del ciclo for

--- gauss.c
+++ gauss.c
@@ -3,9 +3,15 @@
 int main(int argc, char const *argv[])
 {
     /* Suma de Gauss */
-    int n;
+    int n, suma, i;
     printf ("Ingrese un numero entero: ");
     scanf ("%d", &n);
-
+    
+    for ( i = 1; i < n; i++)
+    {
+        suma=suma+i;
+    }
+    printf("Suma total: %d" ,suma);
+    
     return 0;
 }
