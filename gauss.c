ed9f106c7dd54375485f7319ee463f4d213c5089
Laboratorio <laboratorio@udla-ec.int>
2023-10-16 16:07:24 -0500

    Primeras líneas de código

---
+++ gauss.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* Suma de Gauss */
+    int n;
+    printf ("Ingrese un numero entero: ");
+    scanf ("%d", &n);
+
+    return 0;
+}
